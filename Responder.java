0b4faf2b515412db592f53545dae741f0b9d36b7
francisco2016 <otrayama@gmail.com>
2016-07-25 12:30:57 +0200

    -0086 pequeña modificación en el mt generateResponse().

--- Responder.java
+++ Responder.java
@@ -1,5 +1,6 @@
 import java.util.Random;
 import java.util.ArrayList;
+import java.util.HashMap;
 /**
  * The responder class represents a response generator object.
  * It is used to generate an automatic response to an input string.
@@ -11,7 +12,7 @@ public class Responder
 {
     private Random aleatorio;
     private ArrayList<String> cadena;
-    
+    private HashMap<String, String> cadena2;
     /**
      * Construct a Responder - nothing to do
      */
@@ -24,7 +25,13 @@ public class Responder
         cadena.add("3 La capacidad de la menoria hace que su ordenador vaya lento.");
         cadena.add("4 Descargue la aplicación de googleHeart, para verlo.");
         cadena.add("5 Los ordenadores HP  son una buena opción de compra para estos casos.");
-        
+
+        cadena2 = new HashMap<>();
+        cadena2.put("precio", "La avería le costará 50 euros.");
+        cadena2.put("appel", "Son los más caros porque tienen diseño personalizado.");
+        cadena2.put("puertos", "Tiene 6 puertos por defecto, o más si le acopla un adaptador.");
+        cadena2.put("multitarea", "Le recomendamos que instale Linux, ya que windows en multitarea falla.");
+
     }
 
     /**
@@ -33,14 +40,24 @@ public class Responder
      */
     public String generateResponse()
     {
-        int sol = aleatorio.nextInt(cadena.size());
-        return cadena.get(sol);
+        //         int sol = aleatorio.nextInt(cadena.size());
+        //         return cadena.get(sol);
+        /////////////////////////////////////////////////////////////
+        InputReader reader2 = new InputReader();
+        int sol = 0;
+        if(reader2.getInput().contains("precio") || reader2.getInput().contains("appel") || 
+                       reader2.getInput().contains("puertos") ||  reader2.getInput().contains("multitarea")){
+                  sol = aleatorio.nextInt(cadena2.size());          
+        }
+        else{
+            sol = aleatorio.nextInt(cadena2.size());
+        }
+        
+        
+        
+        return sol;
     }
 }
-
-
-
-
 
 
 
